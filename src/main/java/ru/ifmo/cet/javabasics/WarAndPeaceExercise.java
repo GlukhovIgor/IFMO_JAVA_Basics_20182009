9f7c71c6992b1f9e4d1f2e44996d31f10a7adb11
GlukhovIgor <gluhov2000@gmail.com>
2018-10-11 22:01:47 +0300

    made 2nd task

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -2,20 +2,46 @@ package ru.ifmo.cet.javabasics;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
-
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO Iff word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order iff needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
-
-        throw new UnsupportedOperationException();
+        final Charset charset = Charset.forName("windows-1251");
+        List<String> lines12 = Files.readAllLines(tome12Path, charset);
+        List<String> lines34 = Files.readAllLines(tome34Path, charset);
+        List<String> lines = new LinkedList<String>(lines12);
+        lines.addAll(lines34);
+        Map<String, Integer> wmap = new TreeMap<>();
+        String stringlist = lines.toString();
+        stringlist = stringlist.replaceAll("[^a-zA-Zа-яА-Я]", " ").toLowerCase();
+       /* lines = lines.stream()
+                .map(str -> str.toLowerCase())
+                .map(str -> str.replaceAll("[^а-яА-Яa-zA-Z]", " "))
+                .collect(Collectors.toList());
+                */
+        Stream<String> stringStream = Stream.of(stringlist.split(" "));
+        stringStream
+                .filter(word -> word.length() >= 4)
+                .forEach(word -> {
+                    wmap.put(word, wmap.getOrDefault(word, 0) + 1);
+                });
+        Comparator<Map.Entry<String, Integer>> comp = ((pair1, pair2) ->
+                (pair1.getValue().equals(pair2.getValue())) ?
+                        (pair1.getKey().compareTo(pair2.getKey())) : (pair2.getValue().compareTo(pair1.getValue())));
+        String result = wmap.entrySet().stream()
+                .filter(pair -> pair.getValue() >= 10)
+                .sorted(comp)
+                .map(pair -> pair.getKey() + " - " + pair.getValue())
+                .collect(Collectors.joining("\n"));
+        //System.out.println(result);
+        return result;
     }
-
 }
